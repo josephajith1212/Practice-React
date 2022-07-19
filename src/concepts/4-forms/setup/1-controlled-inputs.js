6e6ef8b19ecf558d87bc86f2d02fcb192de0c26e
josephajith1212 <josephajith1212@gmail.com>
2022-07-19 19:45:39 -0400

    form basic

--- src/concepts/4-forms/setup/1-controlled-inputs.js
+++ src/concepts/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,29 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(`Name: ${name}\tEmail: ${email}`)
+  }
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name: </label>
+            <input id='firstName' name='firstName' value={name} onChange={(e)=>setName(e.target.value)}/>
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email: </label>
+            <input id='email' name='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          </div>
+          <button className='btn' type='submit'>Add person</button>
+        </form>
+      </article>
+    </>
+  )
 };
 
 export default ControlledInputs;
