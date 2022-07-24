de149ef83d4a484de54b7d94cf7b48166b813731
josephajith1212 <josephajith1212@gmail.com>
2022-07-23 23:34:54 -0400

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './concepts/7-prop-drilling/setup/1-prop-drilling'
+import Setup from './concepts/9-custom-hooks/setup/1-fetch-example'
 
 function App() {
   return (
--- src/concepts/8-useContext/setup/1-context-api.js
+++ src/concepts/8-useContext/setup/1-context-api.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
+const personContext = React.createContext()
 
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
@@ -11,14 +12,14 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <personContext.Provider value={{removePerson}}>
+      <h3>Context API / useContext</h3>
+      <List people={people} />
+    </personContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
@@ -26,7 +27,6 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +34,8 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const {removePerson} = useContext(personContext)
   return (
     <div className='item'>
       <h4>{name}</h4>
