3877e2041de3dda3c60169742d193e1a4b15142d
josephajith1212 <josephajith1212@gmail.com>
2022-07-23 22:43:21 -0400

    prop-drilling

--- src/concepts/7-prop-drilling/setup/1-prop-drilling.js
+++ src/concepts/7-prop-drilling/setup/1-prop-drilling.js
@@ -5,7 +5,31 @@ import {data} from '../../../data'
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data)
+  return (
+    <section>
+      <h3>prop drilling</h3>
+      <List people={people}/>
+    </section>
+  )
 };
+
+const List = ({people}) => {
+  return (
+    <>
+      {people.map((person) => {
+        return <SinglePerson key={person.id} name={person.name}/>
+      })}
+    </>
+  )
+}
+
+const SinglePerson = ({id, name}) => {
+  return (
+    <div className='item'>
+      <h4>{name}</h4>
+    </div>
+  )
+}
 
 export default PropDrilling;
